effda013ef590b0cb3c9377a26184b15ee2346d4
brothers_trouble <ethan.baraf@gmail.com>
2026-04-30 17:11:45 -0700

    added the client event for the tent render

--- src/main/java/com/unbound/event/ModEventBusEvents.java
+++ src/main/java/com/unbound/event/ModEventBusEvents.java
@@ -1,14 +1,17 @@
 package com.unbound.event;
 
 import com.unbound.BossesUnbound;
+import com.unbound.registry.ModBlockEntities;
 import com.unbound.registry.ModEntities;
 import com.unbound.renderers.GoblinRenderer;
 import com.unbound.entities.GoblinEntity;
 import com.unbound.entities.GoblinKingEntity;
+import com.unbound.renderers.GoblinTentRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 
 @EventBusSubscriber(modid = BossesUnbound.MOD_ID)
@@ -22,6 +25,11 @@ public class ModEventBusEvents {
     }
 
     @SubscribeEvent
+    public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.GOBLIN_TENT.get(), GoblinTentRenderer::new);
+    }
+
+        @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.GOBLIN.get(), GoblinEntity.createAttributes().build());
         event.put(ModEntities.GOBLIN_KING.get(), GoblinKingEntity.createAttributes().build());
